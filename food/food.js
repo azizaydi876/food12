8cd6919667805212c27e45a61ec743af1f15c41e
azizaydi876 <moseleyshane682@gmail.com>
2025-05-27 21:18:05 +0200

    food12

---
+++ food/food.js
@@ -0,0 +1,253 @@
+
+function MakeSlatamechweya(){
+   insatnce.tomato=tomato
+    insatnce.grilled pepper=grilled pepper
+    insatnce.onions=onions
+    instance.salt=salt 
+    insatnce.olive oil=olive oil
+    insatnce.eggs=eggs
+    instance.chicken=chicken
+    instance.grilledtomato=grilledtomato
+    insatnce.grilledonion=grilledonion
+    insatnce.grilledpepper=grilledpeper
+    return instance
+}
+
+       var grilledtomato=function(){
+if (this.tomato===grilled) {
+             console.log("tomato is ready to make grilled salad")
+             }
+            
+        }
+        var grilledonion=function(){
+            if (this.onion===grilled) {
+               console.log("onion is ready to make grilled salad")
+                
+            }
+            
+        }
+         var grilledpeper=function(){
+           if(this.pepper===grilled){
+    console.log("pepper is ready to make grilled salad")
+
+            }
+        }
+        var MakeEggs=function(){
+            if (eggs===8) {
+            console.log( "eggs is ready to make grilled sallad ")
+         }
+        }
+         var  PutSalt=function() {
+            if (sal===1gr) {
+                console.logg("salt is ready to put in slata mechweya")
+                
+            }
+     
+            
+         }
+         var addGrilledSalad=function(){
+            this.tomato=this.tomato+this.GrilledSalad
+            this.onion=this.onion+this.GrilledSalad
+            this.pepper=this.pepper+this.GrilledSalad
+            this.Olive oil=this. Olive oil+this.GrilledSalad
+            this.Salt=this.Salt+this.GrilledSalad
+
+
+
+         }
+
+         
+
+    }
+}
+function MakeTajin() {
+    var instance={}
+    instance.chicken=chicken
+    instance.eggs=8
+    instance.oliveoil=oliveoil
+    insatnce.gratedCheese =gratedCheese
+    instance.addTajin=addTajin 
+    instance.tajin=tajin 
+    return instance}
+
+        var MakeChicken=function() {
+            if (this.chicken===grilled) {
+                console.log("chicken is ready to MakeTajin")
+                
+            }
+            
+        }
+         var MakeEggs=function() {
+            if (this.eggs===8) {
+                console.log("eggs is ready to make tajin")
+
+                
+            }
+         }
+         var MakegratedCheese=function(){
+            if (this.gratedChees===50gr) {
+                console.log("gratedCheese is ready to make tajin")
+                
+            }
+         }
+         var MakeOliveOil=function (){
+            if (this.OliveOil===1/4cup) {
+                console.log("Olive oil is ready to make tajin")
+                
+            }
+         }
+         var addTajin=function(){
+            this.chicken=this.chiken+tajin
+            this.eggs=this.eggs+tajin 
+            this.gratedCheese=this.gratedCheese+tajin
+            this.OliveOil=this.oliveOil+tajin
+         }
+
+         
+        
+    
+
+function MakeSpaghetti(){
+    var instance={}
+    instance.spaghetti=spaghetii
+    instance .tomatoSauce=tomatoSauce
+    instance.salt=salt
+    instance.blackPepper=blackPepper
+    instance.OliveOil=OliveOil
+    instance.clovesGarlicminced=clovesGarlicminced
+    instance.mincemeat=mincemeat 
+    instance.pat=pat 
+    return instance}
+        var PutSalt=function() {
+            if (this.salt===2gr) {
+                console.log("salt is ready to make spaghetti ")
+            }
+            
+        }
+         var MakeBlackPepper=function() {
+              if (this.blackPepper===2gr) {
+                console.log("Black pepper is ready to make spaghetti")
+                
+              }
+            
+         }
+         var MakeOliveOil=function() {
+            if (this.OliveOil===2 tableSpoons) {
+                console.log("Olive Oil is ready to make spaghetti")
+            }
+            
+         }
+         var MakeClovesGarlicminced=function() {
+            if (this.clovesGarlicminced===7) {
+                console.log("cloves Garlic minced is ready to make spaghetti")
+            }
+            
+         }
+         var MakeMinceMeat=function() {
+            if (this.MinceMeat===400gr) {
+                console.log("Mince meat is ready to make spaghetti")
+            }
+            
+         }
+         var MakeTomatoSauce=function() {
+            if (this.tomatoSauce===coocked) {
+                console.log("tomato sauce is ready to make spaghetti")
+            }
+            
+         }
+         var MakePat=function() {
+            if (this.Pat===coocked) {
+                console.log("the Pat of the spaghetti is ready to make spaghetti")
+                
+            }
+            
+         }
+         var addSpaghetti=function() {
+            this.blackPepper=this.blackPepper+sphagetti
+            this.OliveOil=this.OliveOil+spaghetti
+            this.clovesGarlicminced=this.clovesGarlicminced+spaghetti
+            this.MinceMeat=this.MinceMeat+spaghetti 
+            this.TomatoSauce=this.TomatoSauce+spaghetti
+            this.Pat=this.pat+sphagetti
+            
+         }
+         function MakeBorzgen(){
+            var instance={}
+            instance.lambmeat=lambmeat
+            instance.couscous=couscous
+            instance.rosemary=rosemary
+            instance.salt=salt
+            instance.sugar=sugar
+            instance.butter=butter
+            instance.Milk=Milk
+            instance.dates=dates
+            instance.peanuts=peanuts
+            return instance
+
+            }
+            var MakeLambMeat=function(){
+                if (this.lambmeat===700gr) {
+                    console.log("Lamb meat is ready to make Borzgen")
+                }
+            }
+            var MakeCouscous=function() {
+                if (this.Couscous==="750gr") {
+                    console.log("Couscous is ready to make Borzgen")
+                }
+                
+            }
+            var MakeSalt=function() {
+                if (this.salt===1tbsp) {
+                    console.log("Salt is ready to make Borzgen")
+                    
+                }
+                
+            }
+            var MakeSugar=function() {
+                if (this.sugar===100gr) {
+                    console.log("sugar is ready to make Borzgen")
+                }
+                
+            }
+            var MakeButter=function() {
+                if (this.Butter===200gr) {
+                    console.log("Sugar is ready to make Borzgen")
+                }
+                
+            }
+            var MakeMilk=function() {
+                if (this.Milk===250ml) {
+                    console.log("Milk is ready to make Borzgen")
+                }
+                
+            }
+            var MakeDates=function() {
+                if (this.dates===500gr) {
+                    console.log("dates is ready to make Borzgen")
+                }
+                
+            }
+            var MakePeanuts=function() {
+                if (this.Peanuts===250gr) {
+                    console.log("Peanuts is ready to make Borzgen")
+                }
+                
+            }
+            var AddBorzgen=function() {
+                this.lambMeat=this.lambMeat+Borzgen
+                this.Couscous=this.Couscous+Borzgen
+                this.Salt=this.Salt+Borzgen 
+                this.Sugar=this.Sugar+Borzgen
+                this.Butter=this.Butter+Borzgen
+                this.Milk=this.Milk+Borzgen 
+                this.Dates=this.Dates+Borzgen
+                this.Peanuts=this.Peanuts+Borzgen 
+                
+            }
+         }
+
+
+    
+
+
+
